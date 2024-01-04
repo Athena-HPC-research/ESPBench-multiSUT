2daad50fcf0dee6ca95500e83811c98a3a8e6688
TolisSth <tolishalis@gmail.com>
2024-01-04 20:14:50 +0200

    Connector complete

---
+++ connector.sh
@@ -0,0 +1,13 @@
+# Author: Apostolos Halis 2024 <tolishalis@gmail.com>
+#!/bin/bash
+
+# List nodes here
+USER=controller # Please change according to your configuaration
+NODES=("controller@192.168.122.213" "controller@192.168.122.187" "controller@192.168.122.163") 
+
+# Looping through nodes
+for NODE in "${NODES[@]}"; do 
+	scp "./execution.sh" "$NODE:/home/$USER/"
+	ssh $NODE 'bash -s' < "execution.sh"
+done
+
