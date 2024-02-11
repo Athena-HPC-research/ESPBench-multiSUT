788c6a6d23e19b52816ae68950c974b813099422
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2024-02-11 15:47:46 +0200

    Update install-spark.sh

--- install-spark.sh
+++ install-spark.sh
@@ -4,3 +4,4 @@ mv spark-2.4.4-bin-hadoop2.7 spark
 sudo mv spark /opt/
 sudo cp tools/configuration/configurations/spark-2.4.4-bin-hadoop2.7/* /opt/spark/conf/
 echo "export PATH=/opt/spark/bin:\$PATH" >> ~/.bashrc
+source ~/.bashrc
