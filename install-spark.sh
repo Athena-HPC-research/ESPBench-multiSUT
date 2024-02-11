291a5c8b115b13eb0e30013594dd5f4315eb2346
GeorgeFkd <69716466+GeorgeFkd@users.noreply.github.com>
2024-02-11 15:25:53 +0200

    Create install-spark.sh

---
+++ install-spark.sh
@@ -0,0 +1,5 @@
+wget https://archive.apache.org/dist/spark/spark-2.4.4/spark-2.4.4-bin-hadoop2.7.tgz
+tar -xzf spark-2.4.4-bin-hadoop2.7.tgz
+mv spark-2.4.4-bin-hadoop2.7 spark
+sudo mv spark /opt/
+sudo cp tools/configuration/configurations/spark-2.4.4-bin-hadoop2.7/* /opt/spark/conf/
