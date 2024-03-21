396a38b1ac5791edb5bd3a5ceb07f7531a806a8f
George fkd <geonfak76@gmail.com>
2024-03-21 20:00:06 +0200

    Rewrote it using classes for easier maintenance, next up is reading from json,i might have forgotten some ansible things

--- configure.py
+++ configure.py
@@ -5,10 +5,6 @@ def print_string_with_filename_and_separator(filename,content):
     print(f"===========File: {filename} =============")
     print(content)
     print("-------------------------------------------")
-
-
-    
-
 def get_string_from_ip_port_list(ip_port_tuple_list,default_port):
     total_string = ""
     for ip_port in ip_port_tuple_list:
@@ -23,28 +19,10 @@ def get_string_from_ip_port_list(ip_port_tuple_list,default_port):
 def string_array_from(elems):
     total_string = "[" + "\n"
     for i in elems:
-        total_string += "\t" + wrap_string(i) + "\n"
+        total_string += "\t" + wrap_string(i) + "," +"\n"
+    # get rid of the extra comma
+    total_string = total_string[:-2] + "\n"
     return total_string + "\t"+ "]"
-
-
-def datasender_conf(ip_port_tuple_list,input_paths=["~/Benchmarks/ESB/Data/5MinutesMachine1.csv",
-	"~/Benchmarks/ESB/Data/5MinutesMachine2.csv",
-	"~/Benchmarks/ESB/Data/production_times.csv"],read_in_ram=True):
-    #i am including a comma more than needed
-    kafka_bootstrap_servers = get_string_from_ip_port_list(ip_port_tuple_list,9092)
-    input_paths_arr = string_array_from(input_paths)
-    file_content = f"""
-    {property_equals("kafka-producer-config.bootstrap-servers",wrap_string(kafka_bootstrap_servers))}
-    {property_equals("kafka-producer-config.key-serializer-class",wrap_string(kafka_key_serializer_class()))}
-    {property_equals("kafka-producer-config.value-serializer-class",1)}
-    {property_equals("kafka-producer-config.acks",1)}
-    {property_equals("kafka-producer-config.batch-size",16384)}
-    {property_equals("kafka-producer-config.buffer-memory-size",33554432)}
-    {property_equals("kafka-producer-config.linger-time",0)}
-    {property_equals("data-reader-config.data-input-path",input_paths_arr)}
-    {property_equals("data-reader-config.read-in-ram","false")}
-"""
-    return file_content
 
 def generate_file_in(content,path):
     with open(path,'w') as file:
@@ -58,13 +36,8 @@ def obj(obj_to_output,tabs):
         # can add support for more later
         total_string = total_string + (tabs * "\t") + property_equals(property_name,val) + "\n"
     return total_string + ((tabs-1) * "\t") + "}"
-
-
-def db_conf():
-    pass
 def property_equals(property_name, val):
     return f"{property_name} = {val}"
-
 
 def wrap_string(string):
     return f'"{string}"'
@@ -73,121 +46,256 @@ def kafka_key_serializer_class():
 def kafka_value_serializer_class():
     return "org.apache.kafka.common.serialization.StringSerializer"
 
-
-def tpc_gen_properties_conf(number_of_warehouses,output_dir):
-    return f"""
-    {property_equals("NUMBER_OF_WAREHOUSES",number_of_warehouses)}
-    {property_equals("OUTPUT_DIR",output_dir)} 
-    """
-
-def apache_beam_properties(system,parallelism,spark_master,kafka_bootstrap_servers,db_url,db_name,db_user,db_password):
-    spark_m = f"spark://{spark_master}:7077"
-    jdbc_url = f"jdbc:postgresql://{db_url}:5432/{db_name}"
-    kafka_servers = ",".join(list(map(lambda x: f"{x}:9092",kafka_bootstrap_servers)))
-    return f"""
-    {property_equals("system",system)}
-    {property_equals("parallelism",parallelism)}
-    {property_equals("spark-master",wrap_string(spark_m))}
-    {property_equals("kafka-bootstrap-servers",wrap_string(kafka_servers))}
-    {property_equals("db-user",wrap_string(db_user))}
-    {property_equals("db-password",wrap_string(db_password))}
-    """
-print("hello world")
-
-def commons_conf(topic_prefix,benchmark_run,query_configs_arr,kafka_bootstrap_servers,zookeeper_servers,sending_interval=10000000,sending_interval_time_unit="NANOSECONDS",duration=10,duration_time_unit="Minutes"):
-    kafka_servers = ",".join(list(map(lambda x: f"{x}:9092",kafka_bootstrap_servers)))
-    zookeeper = ",".join(list(map(lambda x: f"{x}:2181",zookeeper_servers)))
-    queries = ",\n".join(list(map(lambda x: obj(x,2),query_configs_arr)))
-    query_configs_str = "[" + "\n" + queries + "\n"+ "  ]"
-    return f"""
-    {property_equals("topic-prefix",wrap_string(topic_prefix))}
-    {property_equals("benchmark-run",benchmark_run)}
-    {property_equals("query-configs",query_configs_str)}
-    {property_equals("sending-interval",sending_interval)}
-    {property_equals("sending-interval-time-unit",wrap_string(sending_interval_time_unit))}
-    {property_equals("duration",duration)}
-    {property_equals("duration-time-unit",wrap_string(duration_time_unit))}
-    {property_equals("kafka-bootstrap-servers",wrap_string(kafka_servers))}
-    {property_equals("zookeeper-servers",wrap_string(zookeeper))}
-    """
-
-DRY_RUN = True
-def output_file_in(file_content,path,label):
-    if not DRY_RUN:
-        generate_file_in(file_content,path)
-    else:
-        print(f"Generating in path {path}")
-        print_string_with_filename_and_separator(label,file_content)
-
-
 def ansible_host(machine_name,ip):
     return f"{machine_name} ansible_host={ip}"
-
-def ansible_hosts(spark_ips,kafka_ips,db_ip):
-    index = 1;
-    # need to somehow create a lambda that modifies the index
-    machine_prefix = "bench"
-    spark_master = ansible_host(machine_prefix + "0" + str(index),spark_ips[0])
-    spark_slaves_str = ""
-    for spark_host in spark_ips[1:]:
-        index = index + 1
-        spark_slaves_str = spark_slaves_str + ansible_host(machine_prefix + "0" + str(index),spark_host) + "\n"
-    kafka_str = ""
-    for kafka_host in kafka_ips[1:]:
-        index = index + 1
-        kafka_str = kafka_str + ansible_host(machine_prefix + "0" + str(index),kafka_host) + "\n"
-    index = index + 1
-    db_host = ansible_host(machine_prefix + "0" + str(index),db_ip)
-
-    return f"""
-    [all_nodes:children]
-    streaming_cluster
-    kafka_cluster
-    erp_db
-
-    [streaming_cluster:children]
-    master
-    slaves
-
-    [master]
-    {spark_master}
-
-    [slaves]
-    {spark_slaves_str}
-
-    [kafka_cluster]
-    {kafka_str}
-
-    [erp_db]
-    {db_host}
-    """
 
 def read_from_json(filename):
     return json.load(filename)
 
 
+
+class DatasenderConf:
+    def __init__(self,info_json,write_to_file=False):
+        self.to_file = write_to_file
+        self.json = info_json
+
+        self.file_path = "./tools/datasender/datasender.conf"
+        self.label = "datasender.conf"
+        self.description = "Provides the kafka config to the datasender and the giant csv files that are meant to be streamed"
+
+    def file_content(self,ip_port_tuple_list,input_paths=["~/Benchmarks/ESB/Data/5MinutesMachine1.csv",
+	"~/Benchmarks/ESB/Data/5MinutesMachine2.csv",
+	"~/Benchmarks/ESB/Data/production_times.csv"],read_in_ram=True):
+    #i am including a comma more than needed
+        kafka_bootstrap_servers = get_string_from_ip_port_list(ip_port_tuple_list,9092)
+        input_paths_arr = string_array_from(input_paths)
+        content = f"""
+        kafka-producer-config.bootstrap-servers = {wrap_string(kafka_bootstrap_servers)}
+        kafka-producer-config.key-serializer-class = {wrap_string(kafka_key_serializer_class())}
+        kafka-producer-config.value-serializer-class = {wrap_string(kafka_value_serializer_class())}
+        kafka-producer-config.acks = 1
+        kafka-producer-config.batch-size = 16384
+        kafka-producer-config.buffer-memory-size = 33554432
+        kafka-producer-config.linger-time = 0
+        data-reader-config.data-input-path = {input_paths_arr}
+        data-reader-config.read-in-ram = false
+        """
+        return content
+
+    def read_from_json(self):
+        # in self.json should be the file string, or the parsed thing to only do it once
+        return "",""
+
+    def output_file(self):
+        args_for_file = self.read_from_json()
+        file_content=""
+        if self.to_file:
+            generate_file_in(file_content,self.file_path)
+        else:
+            print_string_with_filename_and_separator(self.label,file_content)
+class DatasenderAppConf:
+    def __init__(self,info_json,write_to_file=False):
+        self.to_file = write_to_file
+        self.json = info_json
+        self.file_path = "./tools/datasender/src/main/resources/application.conf"
+        self.label = "Datasender application.conf"
+        self.description = "Provides the database info for the datasender to connect"
+
+    def file_content(self,db_name,db_user,db_password,server_name,num_threads=20):
+
+        return f"""
+        postgres = {{
+            dataSourceClass = "org.postgresql.ds.PGSimpleDataSource"
+            properties = {{
+                databaseName = "{db_name}"
+                user = "{db_user}"
+                password = "{db_password}"
+                serverName = "{server_name}"
+            }}
+            numThreads = {num_threads}
+        }}
+        """
+
+    def read_from_json(self):
+        # in self.json should be the file string, or the parsed thing to only do it once
+        return "",""
+
+    def output_file(self):
+        args_for_file = self.read_from_json()
+        file_content=""
+        if self.to_file:
+            generate_file_in(file_content,self.file_path)
+        else:
+            print_string_with_filename_and_separator(self.label,file_content)
+
+class TpcGenPropertiesConf:
+    def __init__(self,info_json,write_to_file=False):
+        self.to_file = write_to_file
+        self.json = info_json
+
+        self.file_path = "./tools/tpc-c_gen/tpc-c.properties"
+        self.label = "tpc-c.properties"
+        self.description = "For the generation of the business data it provides the number of warehouses and in what folder to output the data"
+
+    def file_content(self,number_of_warehouses,output_dir):
+        return f"""
+        NUMBER_OF_WAREHOUSES={number_of_warehouses}
+        OUTPUT_DIR={output_dir}
+        """
+
+    def read_from_json(self):
+        # in self.json should be the file string, or the parsed thing to only do it once
+        return "",""
+
+    def output_file(self):
+        args_for_file = self.read_from_json()
+        file_content=""
+        if self.to_file:
+            generate_file_in(file_content,self.file_path)
+        else:
+            print_string_with_filename_and_separator(self.label,file_content)
+
+
+class AnsibleHostsConf:
+    def __init__(self,info_json,write_to_file=False):
+        self.to_file = write_to_file
+        self.json = info_json
+
+        self.file_path = "./tools/configuration/hosts"
+        self.label = "hosts"
+        self.description = "Ansible hosts file, provides the IP for all the subsystems of the benchmark"
+
+    def file_content(self,spark_ips,kafka_ips,db_ip):
+        index = 1;
+        # need to somehow create a lambda that modifies the index
+        machine_prefix = "bench"
+        spark_master = ansible_host(machine_prefix + "0" + str(index),spark_ips[0])
+        spark_slaves_str = ""
+        for spark_host in spark_ips[1:]:
+            index = index + 1
+            spark_slaves_str = spark_slaves_str + ansible_host(machine_prefix + "0" + str(index),spark_host) + "\n"
+        kafka_str = ""
+        for kafka_host in kafka_ips[1:]:
+            index = index + 1
+            kafka_str = kafka_str + ansible_host(machine_prefix + "0" + str(index),kafka_host) + "\n"
+        index = index + 1
+        db_host = ansible_host(machine_prefix + "0" + str(index),db_ip)
+
+        return f"""
+        [all_nodes:children]
+        streaming_cluster
+        kafka_cluster
+        erp_db
+
+        [streaming_cluster:children]
+        master
+        slaves
+
+        [master]
+        {spark_master}
+
+        [slaves]
+        {spark_slaves_str}
+
+        [kafka_cluster]
+        {kafka_str}
+
+        [erp_db]
+        {db_host}
+        """
+
+    def read_from_json(self):
+        # in self.json should be the file string, or the parsed thing to only do it once
+        return "",""
+
+    def output_file(self):
+        args_for_file = self.read_from_json()
+        file_content=""
+        if self.to_file:
+            generate_file_in(file_content,self.file_path)
+        else:
+            print_string_with_filename_and_separator(self.label,file_content)
+
+class ESPBenchCommonsConf:
+    def __init__(self,info_json,write_to_file=False):
+        self.to_file = write_to_file
+        self.json = info_json
+
+        self.file_path = "./tools/commons/commons.conf"
+        self.label = "commons.conf"
+        self.description = "Provides the actual ESPBench configuration, not the tools, but the whole benchmark, how many runs, queries etc."
+
+    def file_content(topic_prefix,benchmark_run,query_configs_arr,kafka_bootstrap_servers,zookeeper_servers,sending_interval=10000000,sending_interval_time_unit="NANOSECONDS",duration=10,duration_time_unit="Minutes"):
+        kafka_servers = ",".join(list(map(lambda x: f"{x}:9092",kafka_bootstrap_servers)))
+        zookeeper = ",".join(list(map(lambda x: f"{x}:2181",zookeeper_servers)))
+        queries = ",\n".join(list(map(lambda x: obj(x,2),query_configs_arr)))
+        query_configs_str = "[" + "\n" + queries + "\n"+ "  ]"
+        return f"""
+        topic-prefix = {wrap_string(topic_prefix)}
+        benchmark-run = {benchmark_run}
+        query-configs = {query_configs_str}
+        sending-interval = {sending_interval}
+        sending-interval-time-unit = {wrap_string(sending_interval_time_unit)}
+        duration = {duration}
+        duration-time-unit = {wrap_string(duration_time_unit)}
+        kafka-bootstrap-servers = {wrap_string(kafka_servers)}
+        zookeeper-servers = {wrap_string(zookeeper)}
+        """
+
+    def read_from_json(self):
+        # in self.json should be the file string, or the parsed thing to only do it once
+        return "",""
+
+    def output_file(self):
+        args_for_file = self.read_from_json()
+        file_content=""
+        if self.to_file:
+            generate_file_in(file_content,self.file_path)
+        else:
+            print_string_with_filename_and_separator(self.label,file_content)
+
+class ApacheBeamConf:
+    def __init__(self,info_json,write_to_file=False):
+        self.to_file = write_to_file
+        self.json = info_json
+        self.file_path = "./implementation/beam/src/main/resources/beam.properties"
+        self.label = "beam.properties"
+        self.description = "Provides all the needed data to the SUT(db->url,user,password,kafka-servers->IPs) and what system to use(Spark,HazelCast) and where is the master of that system->IP"
+
+    def file_content(self,system,parallelism,spark_master,kafka_bootstrap_servers,db_url,db_name,db_user,db_password):
+    #i am including a comma more than needed
+        spark_m = f"spark://{spark_master}:7077"
+        jdbc_url = f"jdbc:postgresql://{db_url}:5432/{db_name}"
+        kafka_servers = ",".join(list(map(lambda x: f"{x}:9092",kafka_bootstrap_servers)))
+        return f"""
+        system = {system}
+        parallelism = {parallelism}
+        spark-master = {wrap_string(spark_m)}
+        kafka-bootstrap-servers = {wrap_string(kafka_servers)}
+        db-user = {wrap_string(db_user)}
+        db-password = {wrap_string(db_password)}
+        """
+
+    def read_from_json(self):
+        # in self.json should be the file string, or the parsed thing to only do it once
+        return "",""
+
+    def output_file(self):
+        args_for_file = self.read_from_json()
+        file_content=""
+        if self.to_file:
+            generate_file_in(file_content,self.file_path)
+        else:
+            print_string_with_filename_and_separator(self.label,file_content)
+
 def main():
-    config_data = read_from_json(sys.argv[1])
-    ds_conf = datasender_conf([("192.168.69.4")])
-    ds_conf_path = "./tools/datasender/datasender.conf"
-    output_file_in(ds_conf,ds_conf_path,"datasender.conf")
-
-    tpc_gen_file = tpc_gen_properties_conf(3,"data")
-    tpc_gen_file_path = "./tools/tpc-c_gen/tpc-c.properties"
-    output_file_in(tpc_gen_file, tpc_gen_file_path,"tpc-c.properties")
-
-    apache_beam_file = apache_beam_properties("spark",1,"192.168.68.3",["192.168.69.4"],"192.168.69.5","hessebench","benchmarker","benchmark")
-    apache_beam_file_path = "./implementation/beam/src/main/resources/beam.properties"
-    output_file_in(apache_beam_file,apache_beam_file_path,"beam.properties")
-
-    commons_conf_file = commons_conf("HPC-TEAM",1,[{"name":"Identity","number-of-streams":1}],["192.168.69.4"],["192.168.69.4"])
-    commons_conf_file_path = "./tools/commons/commons.conf"
-    output_file_in(commons_conf_file,commons_conf_file_path,"commons.conf")
-
-    ansible_hosts_file = ansible_hosts(["192.168.69.3","192.168.69.4"],["192.168.69.5","192.168.69.6"],"192.168.69.6")
-    ansible_hosts_file_path = "./tools/configuration/hosts"
-    output_file_in(ansible_hosts_file,ansible_hosts_file_path,"hosts")
-    
+    ds_conf = DatasenderConf("")
+    content = ds_conf.file_content([("192.168.69.4")])
+    tpc_conf = TpcGenPropertiesConf("")
+    content = tpc_conf.file_content(3,"data")
+    print(content)
+    esp_conf = ESPBenchCommonsConf("")
+    ansible_conf = AnsibleHostsConf("")
 main()
 
 
