ad4d6f7f37c43f2355ca9f4c822e5d44d0cb2a39
George fkd <geonfak76@gmail.com>
2024-04-05 21:03:29 +0300

    now installing scala works on both x86 and arm (64bits both ofc)

--- install-scala.sh
+++ install-scala.sh
@@ -1,4 +1,17 @@
-curl -fL https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz | gzip -d > cs
+
+ARCH=$(uname -m)
+echo "System architecture is: $ARCH"
+
+if [ $ARCH == "x86_64" ]; then
+	SCALADOWNLOAD=https://github.com/coursier/coursier/releases/latest/download/cs-x86_64-pc-linux.gz
+elif [ $ARCH == "arm64" ]; then
+	SCALADOWNLOAD=https://github.com/VirtusLab/coursier-m1/releases/latest/download/cs-aarch64-pc-linux.gz
+fi
+
+echo "Downloading from: $SCALADOWNLOAD"
+# this command never works properly
+curl -fL "$SCALADOWNLOAD" | gzip -d > cs && chmod +x cs && ./cs setup
+
 chmod +x cs && ./cs setup
 cs install scala:2.12.8
 printf "Installed scala and sbt with coursier cli\n"
